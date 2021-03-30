e091996741717a1a7acaf056eb99675e2110fc02
scaunasu.claudia.h9s@robotics.ucv.ro <scaunasu.claudia.h9s@robotics.ucv.ro>
2021-03-30 11:19:45 +0300

    Changing page of login

---
+++ LibraryProject/LibraryApp/Login.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibraryApp.LibraryServiceReference;
+
+namespace LibraryApp
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        private ServiceClient _serviceClient = new ServiceClient();
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void BtnLogin_OnClick(object sender, RoutedEventArgs e)
+        {
+            var result = _serviceClient.LogIn(TxtUsername.Text, TxtPassword.Password);
+
+            MessageBox.Show(result ? "success" : "Failed");
+        }
+
+
+        private void BtnExit_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            Application.Current.Shutdown();
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
