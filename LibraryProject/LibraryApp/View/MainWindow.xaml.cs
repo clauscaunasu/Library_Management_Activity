a3d23ca296475c7b1dea42e25fbff7c4f21fdb84
scaunasu.claudia.h9s@robotics.ucv.ro <scaunasu.claudia.h9s@robotics.ucv.ro>
2021-03-31 16:54:10 +0300

    Solving the dragmove() problem and creating a folder for the Graphical UI

---
+++ LibraryProject/LibraryApp/View/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using LibraryApp.LibraryServiceReference;
+using MaterialDesignThemes.Wpf;
+
+
+namespace LibraryApp.View
+{
+
+    public partial class MainWindow : Window
+    {
+        private ServiceClient _serviceClient = new ServiceClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnGoToLogin_OnClick(object sender, RoutedEventArgs e)
+        {
+            var loginWindow = new LoginUser();
+            this.Visibility = Visibility.Hidden;
+            loginWindow.Show();
+        }
+
+        private void BtnRegister_OnClick(object sender, RoutedEventArgs e)
+        {
+            var registerWindow = new RegisterUser();
+            this.Visibility = Visibility.Hidden;
+            registerWindow.Show();
+
+        }
+
+
+        private void BtnExit_OnClick(object sender, RoutedEventArgs e)
+        {
+
+            Application.Current.Shutdown();
+        }
+
+        private void UIElement_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
